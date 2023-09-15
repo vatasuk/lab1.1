97ea86dae96cc978e1c837915741750f8593b610
vatasuk <vatasuk111@gmail.com>
2023-09-15 15:12:11 +0300

    пап

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,42 +2,29 @@ package org.example;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        List<City> cities = new ArrayList<>();
-        cities.add(new City(1,"Ростов-на-Дону",1000000, 2));
-        cities.add(new City(2,"Азов",80000,1));
-        cities.add(new City(3,"Новочеркаск", 100000,1));
+        Repos repos = new Repos();
+        ArrayList<City> cities = new ArrayList<>();
 
-        List<Park> parks = new ArrayList<>();
-        parks.add(new Park(1,1,2500,0,1,"gfgf","14.05.1978"));
-        parks.add(new Park(1,1,2500,0,1,"fgfhb","21.08.1978"));
-        parks.add(new Park(2,3,500,1,2,"dfd","15.10.1998"));
-        parks.add(new Park(3,2,1500,1,1,"xvc","27.03.2005"));
+        cities.add(new City(2, "Азов", 80000, 1));
+        cities.add(new City(1, "Ростов-на-Дону", 1000000, 2));
+        cities.add(new City(3, "Новочеркаск", 100000, 1));
 
-        for (int i = 0; i < cities.size(); i ++)
-        {
-            String st = status(cities.get(i).getStatus());
-            System.out.printf("Название города - %s, население - %d, статус - %s \n", cities.get(i).getName(), cities.get(i).getPop(), st);
-            for (int j = 0; j < parks.size(); j++)
-            {
-                if (cities.get(i).getId() == parks.get(i).getCityID())
-                {
+        ArrayList<Park> parks = new ArrayList<>();
+        parks.add(new Park(1, 1, 2500, 0, 1, "gfgf", "14.05.1978"));
+        parks.add(new Park(1, 1, 2500, 0, 1, "fgfhb", "21.08.1978"));
+        parks.add(new Park(2, 3, 500, 1, 2, "dfd", "15.10.1998"));
+        parks.add(new Park(3, 2, 1500, 1, 1, "xvc", "27.03.2005"));
 
-                }
+        repos.pr(cities,parks);
+
+        Collections.sort(cities, new Comparator<City>() {
+            public int compare(City o1, City o2) {
+                int a = o1.getPop();
+                int b = o2.getPop();
+                return Integer.compare(b, a);
             }
-        }
-
-
-    }
-
-    private static String status(int id) {
-        String stat = null;
-        switch (id)
-        {
-            case 1: stat = "Город";
-            break;
-            case 2: stat = "Город-миллионик";
-            break;
-        }
-        return stat;
+        });
+        repos.pr(cities);
     }
 }
+
---
+++ src/main/java/org/example/Repos.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.awt.*;
+import java.util.List;
+
+public class Repos {
+
+    public void pr(List<City> cities, List<Park> parks) {
+        for (int i = 0; i < cities.size(); i++) {
+            String st = status(cities.get(i).getStatus());
+            System.out.printf("Название города - %s, население - %d, статус - %s \n", cities.get(i).getName(), cities.get(i).getPop(), st);
+            System.out.println("Парки в этом городе:");
+            for (int j = 0; j < parks.size(); j++) {
+                if (cities.get(i).getId() == parks.get(j).getCityID()) {
+                    System.out.printf("Название парка - %s, Площадь - %d, наличие водоемов - %s, тип парка - %s, дата основания - %s \n ", parks.get(j).getName(),
+                            parks.get(j).getS(), water(parks.get(j).getWater()), type(parks.get(j).getType()), parks.get(j).getDate());
+                }
+            }
+        }
+    }
+    public void pr(List<City> cities) {
+        for (int i = 0; i < cities.size(); i++) {
+            String st = status(cities.get(i).getStatus());
+            System.out.printf("Название города - %s, население - %d, статус - %s \n", cities.get(i).getName(), cities.get(i).getPop(), st);
+            System.out.println("Парки в этом городе:");
+        }
+    }
+
+
+
+    private  String status(int id) {
+        String stat = null;
+        switch (id)
+        {
+            case 1: stat = "Город";
+                break;
+            case 2: stat = "Город-миллионик";
+                break;
+        }
+        return stat;
+    }
+    private  String water(int id)
+    {
+        String wt = null;
+        switch (id)
+        {
+            case 1: wt = "Есть";
+                break;
+            case 2: wt = "Нет";
+                break;
+        }
+        return wt;
+    }
+    private  String type(int id)
+    {
+        String t = null;
+        switch (id)
+        {
+            case 1: t = "vb";
+                break;
+            case 2: t = "ghg";
+                break;
+        }
+        return t;
+    }
+
+}
